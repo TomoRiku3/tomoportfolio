0d07f961f6abf9d130f20e9bf394fc8539478b93
rikutomo <tomo.riku@mail.utoronto.ca>
2025-01-09 12:37:49 +0900

    Made Edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="App-nav">
-          <ul>
-            <li>
-              <Link to="/">Projects</Link>
-            </li>
-            <li>
-              <Link to="/resume">Resume</Link>
-            </li>
-          </ul>
-        </nav>
+        <ul className="navbar">
+          <li><Link to="/">Project</Link></li>
+          <li><Link to="/resume">Resume</Link></li>
+        </ul>
         <Routes>
-          <Route path="/" element={<Projects />} />
+          <Route path="/" element={<Project />} />
           <Route path="/resume" element={<Resume />} />
         </Routes>
       </div>
@@ -25,19 +19,26 @@ function App() {
   );
 }
 
-function Projects() {
+function Project() {
   return (
-    <div className="App-content">
+    <div>
       <h1>Tomo's Portfolio</h1>
+      <div className="scroll-container">
+        <div className="scroll-item">Project #1 soon...</div>
+        <div className="scroll-item">Project #2 soon...</div>
+        <div className="scroll-item">Project #3 soon...</div>
+        <div className="scroll-item">Project #4 soon...</div>
+        <div className="scroll-item">Project #5 soon...</div>
+      </div>
     </div>
   );
 }
 
 function Resume() {
   return (
-    <div className="App-content">
+    <div>
       <h1>Resume</h1>
-      <p>Here you can add your resume content.</p>
+      <p>This is the resume page.</p>
     </div>
   );
 }
