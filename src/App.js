cdd78bca44c9d7c79802b97f4ed2b6dacdecc570
rikutomo <tomo.riku@mail.utoronto.ca>
2025-01-17 15:25:49 -0500

    change to be discarded

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Project />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/project1" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
+          <Route path="/project3" element={<Project3 />} />
+          <Route path="/project4" element={<Project4 />} />
+          <Route path="/project5" element={<Project5 />} />
         </Routes>
       </div>
     </Router>
@@ -24,11 +29,11 @@ function Project() {
     <div>
       <h1>Tomo's Portfolio</h1>
       <div className="scroll-container">
-        <div className="scroll-item">Project #1 soon...</div>
-        <div className="scroll-item">Project #2 soon...</div>
-        <div className="scroll-item">Project #3 soon...</div>
-        <div className="scroll-item">Project #4 soon...</div>
-        <div className="scroll-item">Project #5 soon...</div>
+        <Link to="/project1" className="scroll-item">Project #1 soon...</Link>
+        <Link to="/project2" className="scroll-item">Project #2 soon...</Link>
+        <Link to="/project3" className="scroll-item">Project #3 soon...</Link>
+        <Link to="/project4" className="scroll-item">Project #4 soon...</Link>
+        <Link to="/project5" className="scroll-item">Project #5 soon...</Link>
       </div>
     </div>
   );
@@ -41,6 +46,26 @@ function Resume() {
       <p>This is the resume page.</p>
     </div>
   );
+}
+
+function Project1() {
+  return <h1>Project 1</h1>;
+}
+
+function Project2() {
+  return <h1>Project 2</h1>;
+}
+
+function Project3() {
+  return <h1>Project 3</h1>;
+}
+
+function Project4() {
+  return <h1>Project 4</h1>;
+}
+
+function Project5() {
+  return <h1>Project 5</h1>;
 }
 
 export default App;
